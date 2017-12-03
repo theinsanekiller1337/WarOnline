2656dc64c1af93af8a375ebec12442c30391a31f
TopGameStudios <30956325+TopGameStudios@users.noreply.github.com>
2017-12-03 16:34:58 +0530

    Project-WO-1
    The First project file.

---
+++ War Online/Assets/Game Scripts/Movement.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+
+function Update () {
+
+
+     var xAxisValue = Input.GetAxis("Horizontal");
+     var zAxisValue = Input.GetAxis("Vertical");
+
+      transform.Translate(xAxisValue*8,0.0f,zAxisValue*8);
+
+      var xyAxisValue = Input.GetAxis("Zoom_Out");
+
+      transform.Translate(xyAxisValue*8,xyAxisValue*8,0.0f);
+
+      var rotateValue = Input.GetAxis("Rotate");
+
+      transform.Rotate(0.0f, rotateValue*8,0.0f);
+  
+
+ }
