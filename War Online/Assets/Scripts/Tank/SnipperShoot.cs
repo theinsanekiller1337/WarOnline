0a116d9591dd9210ed3910e8f8bf3283a2eb2d12
Srijan Gods <srijagod10@gmail.com>
2017-12-16 21:11:26 +0530

    Game-V2
    Yo Sniper Done

---
+++ War Online/Assets/Scripts/Tank/SnipperShoot.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class SnipperShoot : MonoBehaviour {
+
+	public float damage = 10f;
+	public float impactForce = 100f;
+
+	public GameObject fpsCam;
+	public ParticleSystem muzzleFlash;
+	public GameObject impactEffect;
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetButtonUp("Fire")){
+		
+		  Shoot();
+
+		}
+
+	}
+
+	void Shoot () {
+
+	muzzleFlash.Play();
+	
+	 RaycastHit hit;
+
+	 if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit)){
+	 
+	    Debug.Log(hit.transform.name);
+
+		TankHealth target = hit.transform.GetComponent<TankHealth>();
+
+		if(target != null){
+		
+		target.TakeDamage(damage);
+
+		}
+
+		if (hit.rigidbody != null){
+		
+		hit.rigidbody.AddForce(-hit.normal * impactForce);
+
+		}
+
+		GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(-hit.normal * impactForce ));
+		Destroy(impactGO, 2f);
+
+	 }
+
+	}
+}
