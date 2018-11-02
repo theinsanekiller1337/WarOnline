d18180021d9ccf8a9939e4add31b24165c5d76c0
Srijan Gods <srijagod10@gmail.com>
2018-11-02 20:48:09 +0530

    Massive UI Addition and FluidParticle Package
    The whole game UI can be made, I would like @ermderp24 to make changes to ui ASAP. Also to run the turret Acidton, added Fluid Particle thrower, not it really looks like an acid.

---
+++ War Online/Assets/Plugins/UI-Base/Scripts/LaunchURL.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaunchURL : MonoBehaviour {
+
+	public string URLi; 
+
+	public void urlLinkOrWeb() 
+	{
+		Application.OpenURL(URLi);
+	}
+}
